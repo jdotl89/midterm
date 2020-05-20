6d9f22894a63ae8cf14a3746316e9d3b83586e69
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-20 18:07:49 -0400

    added summary

--- index.php
+++ index.php
@@ -20,9 +20,50 @@ $f3->route('GET /', function(){
 });
 
 // breakfast route
-$f3->route('GET /survey', function(){
+$f3->route('GET|POST /survey', function($f3){
 
     $checkbox = $GLOBALS['controller']->getCheckbox();
+
+    echo '<h1>Midterm Survey</h1>';
+    echo '<pre>';
+    echo '<form action="#" method="post"><label for="first"><strong>Name</strong></label> ';
+    echo '<input type="text" id="first" name="name" value="';
+    if (isset($_POST['name'])) {
+        echo $_POST['name'];
+    }
+    echo '"><br><br>';
+    echo'<label ><strong>Check all that apply</strong></label><br><br>';
+    for($i = 0; $i < sizeof($checkbox); $i++) {
+        echo '<input  type="checkbox" name= "check[]" value="'.$checkbox[$i].' ">' . " ".$checkbox[$i].'</input>';
+        echo '<br><br>';
+    }
+    echo '<button type="submit">Submit</button></form>';
+    echo '</pre>';
+    echo '';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['name'] = $_POST['name'];
+        $_SESSION['checkbox'] = $_POST['check'];
+
+        //Redirect to Order 2 page
+        $f3->reroute('/summary');
+    }
+
+    /*    $view = new Template();
+        echo $view->render('views/home.html');*/
+});
+
+$f3->route('GET /summary', function(){
+
+    echo '<h1>Midterm Survey</h1>';
+    echo '<pre>';
+    echo '<p>Thank you for taking the survey, '.$_SESSION['name'].'!</p>';
+    echo '<p><strong>Your Answers:</strong></p>';
+    echo '<p>'.implode($_SESSION['checkbox'], ", ").'</p>';
+    echo '</pre>';
+/*
 
     echo '<h1>Midterm Survey</h1>';
 
@@ -36,10 +77,11 @@ $f3->route('GET /survey', function(){
     }
     echo '<button type="submit">Submit</button></form>';
     echo '</pre>';
-    echo '';
+    echo '';*/
 
     /*    $view = new Template();
         echo $view->render('views/home.html');*/
+    session_destroy();
 });
 
 // run fat free
