beb1bd03b76fce8dab3fe83fd74a00c4c1e5b53c
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-20 16:28:35 -0400

    created index and composer files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function(){
+    echo '<h1>Midterm Survey</h1>';
+    echo '<a href="#">Take my Midterm Survey</a>';
+});
+
+// breakfast route
+$f3->route('GET /breakfast', function(){
+    echo '<h1>Hello world!</h1>';
+
+    /*    $view = new Template();
+        echo $view->render('views/home.html');*/
+});
+
+// run fat free
+$f3->run();
